9fc87989f9c73296947e364dc489c4c4cc51f0c1
ryancan <ryanvcan@gmail.com>
2016-09-10 17:43:07 -0400

    Update sketch.js

--- torque-on-wrench/sketch.js
+++ torque-on-wrench/sketch.js
@@ -4,28 +4,21 @@ function setup(){
   createCanvas(900,900);
   background('white');
 
+//Create some vectors, arrows, and a slider
   center = createVector(150,275);
   pointApplied_x = createSlider(0,300,300,10);
   pointApplied_x.position(300,100,200);
   end = createVector(450,450);
   pointApplied = createVector(center.x+pointApplied_x.value(),center.y);
   momentArm = new Arrow(center,pointApplied);
-  momentArm.color = color('blue');
-  momentArm.width = 10;
-  momentArm.showComponents = false;
-  momentArm.draggable = false;
-  momentArm.grab = false;
-  force1 = new Arrow(pointApplied,end);
-  force1.color = color('red');
-  force1.grab = true;
-  force1.draggable = false;
-  force1.showComponents = true;
+  force1 = new Arrow(momentArm.target,end);
+
 }
 
 function draw(){
   background('white');
 
-
+//Draw wrench and bolt
   fill('grey');
   rect(200,250,300,50);
   polygon(center.x,center.y,80,6);
@@ -36,11 +29,26 @@ function draw(){
   polygon(center.x,center.y,30,6);
 
   stroke(1);
+  pointApplied = createVector(center.x+pointApplied_x.value(),center.y);
+
+  momentArm = new Arrow(center,pointApplied);
+  momentArm.color = color('blue');
+  momentArm.width = 10;
+  momentArm.showComponents = false;
+  momentArm.draggable = false;
+  momentArm.grab = false;
+  force1.origin.x = pointApplied.x;
+  force1.color = color('red');
+  force1.grab = true;
+  force1.draggable = false;
+  force1.showComponents = true;
 
   momentArm.update();
   force1.update();
   momentArm.display();
   force1.display();
+
+  //Calculate Torque
   torque = (force1.target.y-force1.origin.y).toFixed(0)*pointApplied_x.value()/10;
 
   fill('black');
@@ -48,6 +56,8 @@ function draw(){
   text("The red arrow is the force vector",50,35);
   text("Torque= "+torque.toFixed(0)+" N-cm",500,400);
   text("Moment Arm Length= "+(pointApplied_x.value()/10).toFixed(0)+" cm",400,100);
+
+
   if (torque > 0){
     text("The bolt will tighten",200,400);
   } else if (torque < 0){
