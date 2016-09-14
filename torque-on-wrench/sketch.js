c6690bab0557e7b23a5848ed2632c1daae0f8197
ryancan <ryanvcan@gmail.com>
2016-09-13 21:57:09 -0400

    fixed force vector
    fixed the force vector to keep its magnitude and direction while using slider to change moment arm length

--- torque-on-wrench/sketch.js
+++ torque-on-wrench/sketch.js
@@ -6,7 +6,7 @@ function setup(){
 
 //Create some vectors, arrows, and a slider
   center = createVector(150,275);
-  pointApplied_x = createSlider(0,300,300,10);
+  pointApplied_x = createSlider(0,350,300,10);
   pointApplied_x.position(300,100,200);
   end = createVector(450,450);
   pointApplied = createVector(center.x+pointApplied_x.value(),center.y);
@@ -17,7 +17,7 @@ function setup(){
 
 function draw(){
   background('white');
-
+  strokeWeight(1);
 //Draw wrench and bolt
   fill('grey');
   rect(200,250,300,50);
@@ -25,8 +25,11 @@ function draw(){
   fill('white');
   noStroke();
   rect(70,250,110,50);
-  fill(150,20,40);
+  fill('gray');
+  stroke(1);
   polygon(center.x,center.y,30,6);
+  fill('white');
+  ellipse(center.x,center.y,30,30);
 
   stroke(1);
   pointApplied = createVector(center.x+pointApplied_x.value(),center.y);
@@ -37,11 +40,22 @@ function draw(){
   momentArm.showComponents = false;
   momentArm.draggable = false;
   momentArm.grab = false;
+
+//save old value to use for compensating for slider change
+  oldX = force1.origin.x;
+
   force1.origin.x = pointApplied.x;
   force1.color = color('red');
   force1.grab = true;
   force1.draggable = false;
   force1.showComponents = true;
+
+
+//trying to keep integrity of force vector while enduring slider change
+  if (mouseIsPressed){
+    dx = force1.origin.x - oldX;
+    force1.target.x = force1.target.x + dx;
+  };
 
   momentArm.update();
   force1.update();
@@ -54,17 +68,30 @@ function draw(){
   fill('black');
   text("The blue arrow is the position vector corresponding to the moment arm",50,20);
   text("The red arrow is the force vector",50,35);
+  text("Notice the sense change in torque vector",50,50);
   text("Torque= "+torque.toFixed(0)+" N-cm",500,400);
   text("Moment Arm Length= "+(pointApplied_x.value()/10).toFixed(0)+" cm",400,100);
 
 
+
   if (torque < 0){
-    text("The bolt will tighten (Negative Toqrue, going into screen along -z axis)",50,400);
+    //text("The bolt will tighten (Negative Toqrue, going into screen along -z axis)",50,400);
+    //ellipse(center.x,center.y,30,30);
+    strokeWeight(3);
+    line(center.x-10,center.y-10,center.x+10,center.y+10);
+    line(center.x-10,center.y+10,center.x+10,center.y-10);
+    noFill();
+    arc(center.x, center.y, 30, 30, 0, TWO_PI);
+
   } else if (torque > 0){
-    text("The bolt will loosen(Positive Torque, coming out of screen along +z axis)",50,400);
+  //  text("The bolt will loosen(Positive Torque, coming out of screen along +z axis)",50,400);
+    strokeWeight(3);
+    ellipse(center.x, center.y, 5, 5);
+    noFill();
+    arc(center.x, center.y, 30, 30, 0, TWO_PI);
   } else{
-    text("The bolt will not move! No moment applied!",50,400);
-}
+  // text("The bolt will not move! No moment applied!",50,400);
+  }
 }
 
 //taken from p5.js examples
