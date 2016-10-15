f1e7129a9d4db7dbba21530ae74cdc0aed9e0821
ryancan <ryanvcan@gmail.com>
2016-10-15 14:05:11 -0400

    added bouncing and comments
    added in some simple bouncing and code commentary

--- projectile/sketch.js
+++ projectile/sketch.js
@@ -15,38 +15,48 @@ fetch.mousePressed(fetchBall);
 
 initialPos = createVector(50,340);
 initialVel = createVector(0,0);
-no_g = createVector(0,0);
+no_g = createVector(0,0); //a zero vector used to keep the ball stationary
 projectile = new KineticMass(initialPos, initialVel, no_g, 20, 'orange');
-projectile.tail = true;
-projectile.limit = 10;
+projectile.tail = true; //enabled to track projectile motion
+projectile.limit = 10; //used to keep projectile visible on screen if the user tries to apply too large an initial velocity
 projectile.tailFill = color('white');
 projectile.tailStroke = color('white');
 
+//create arrow objects to represent vectors based off of the projectiles actual velocity and acceleratiion
 
 velVec = new Arrow(projectile.position,p5.Vector.add(projectile.position,projectile.velocity));
 velVec.color = color('red');
-velVec.grab = true;
+velVec.grab = true; //enables user to grab vector tip to change initial applied velocity
 velVec.draggable = false;
-velVec.width = 10;
+velVec.width = 10;//make the arrow vector objects a little thinner to have comparable size with projectile
 
 gravVec = new Arrow(projectile.position,p5.Vector.add(projectile.position,projectile.acceleration));
 gravVec.color = color('purple');
-gravVec.grab = false;
+gravVec.grab = false; // booleans set to false so that user cannot affect gravitational force
 gravVec.draggable = false;
-gravVec.width = 10;
+gravVec.width = 10;//make the arrow vector objects a little thinner to have comparable size with projectile
 
 }
 
 function draw(){
 background('white');
 fill('green');
-rect(0,349,720,50);
+rect(0,349,720,50); //scenic grass
 fill('blue');
-rect(0,0,720,350);
+rect(0,0,720,350); //scenic sky
 
+//Conditional used to induce bouncing
+if (projectile.position.y > 340){
+  projectile.velocity.y *= -.8;
+  projectile.position.y = 340;
+};
+
+//update prjectile before it's corresponding arrow vectors so that the arrows reflect the current values
 projectile.update();
 projectile.display();
 
+//this conditional allows the velocity arrow vector to only be able to be grabbed and changed
+//when the projectile is in the initial starting position
 if (projectile.velocity.x == 0 && projectile.velocity.y == 0){
   velVec.grab = true;
 }else{
@@ -54,10 +64,10 @@ if (projectile.velocity.x == 0 && projectile.velocity.y == 0){
   velVec.grab = false;
   gravVec = new Arrow(projectile.position,p5.Vector.add(projectile.position,projectile.acceleration));
 
-  push();
-  velVec.target = p5.Vector.add(projectile.position,p5.Vector.mult(projectile.velocity,10));
-  gravVec.target = p5.Vector.add(projectile.position,p5.Vector.mult(projectile.acceleration,300));
-  pop();
+  //scale the velocity and gravity arrow vectors to make visible to the user
+  velVec.target = p5.Vector.add(projectile.position,p5.Vector.mult(projectile.velocity,5));
+  gravVec.target = p5.Vector.add(projectile.position,p5.Vector.mult(projectile.acceleration,200));
+
 };
 
 velVec.color = color('red');
@@ -65,21 +75,21 @@ gravVec.color = color('purple');
 velVec.width = 10;
 gravVec.width = 10;
 
-
 gravVec.update();
 gravVec.display();
 velVec.update();
 velVec.display();
-
-
 }
 
+//function to apply initial velocity to stationary projectile based off of user input dragged velocity Vector
+//function also applies gravity as there is no real ground providing a normal force on the ball to keep stationary
 function initiate(){
   projectile.velocity = createVector(velVec.target.x-velVec.origin.x, velVec.target.y-velVec.origin.y);
-  p5.Vector.mult(projectile.velocity,.01);
   projectile.acceleration = createVector(0,.981/5);
 }
 
+//function that resets ball after being launched to original position and sets velocity and acceleration to zero
+//function also re-enables the velocity vector tip to be grabbed to change the initial velocity that will be applied using the initiate button
 function fetchBall() {
   projectile.position.x = initialPos.x;
   projectile.position.y = initialPos.y;
