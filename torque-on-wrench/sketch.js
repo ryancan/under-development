76d747a3e5915170e6f7574cc32b61b17200363b
ryancan <ryanvcan@gmail.com>
2016-10-18 21:00:14 -0400

    changed labels/math
    Changed moment arm label to position vector. Added calculation of phi, angle between vectors. Changed calculation of torque to use sin(phi). Added in vector labels of (r) and (F). Removed instructions.

--- torque-on-wrench/sketch.js
+++ torque-on-wrench/sketch.js
@@ -7,11 +7,11 @@ function setup(){
 //Create some vectors, arrows, and a slider
   center = createVector(150,275);
   pointApplied_x = createSlider(0,350,300,10);
-  pointApplied_x.position(300,100,200);
+  pointApplied_x.position(80,100,200);
   end = createVector(450,450);
   pointApplied = createVector(center.x+pointApplied_x.value(),center.y);
-  momentArm = new Arrow(center,pointApplied);
-  force1 = new Arrow(momentArm.target,end);
+  positionVector = new Arrow(center,pointApplied);
+  force1 = new Arrow(positionVector.target,end);
 
 }
 
@@ -27,19 +27,22 @@ function draw(){
   rect(70,250,110,50);
   fill('gray');
   stroke(1);
-  polygon(center.x,center.y,30,6);
+  polygon(center.x,center.y,28,6);
   fill('white');
   ellipse(center.x,center.y,30,30);
+  line(85,250,180,250);
+  line(85,300,180,300);
+  line(180,250,180,300);
 
   stroke(1);
   pointApplied = createVector(center.x+pointApplied_x.value(),center.y);
 
-  momentArm = new Arrow(center,pointApplied);
-  momentArm.color = color('blue');
-  momentArm.width = 10;
-  momentArm.showComponents = false;
-  momentArm.draggable = false;
-  momentArm.grab = false;
+  positionVector = new Arrow(center,pointApplied);
+  positionVector.color = color('blue');
+  positionVector.width = 10;
+  positionVector.showComponents = false;
+  positionVector.draggable = false;
+  positionVector.grab = false;
 
 //save old value to use for compensating for slider change
   oldX = force1.origin.x;
@@ -48,35 +51,39 @@ function draw(){
   force1.color = color('red');
   force1.grab = true;
   force1.draggable = false;
-  force1.showComponents = true;
+  force1.showComponents = false;
 
 
-//trying to keep integrity of force vector while enduring slider change
+//keeping integrity of force vector while enduring slider change
   if (mouseIsPressed){
     dx = force1.origin.x - oldX;
     force1.target.x = force1.target.x + dx;
   };
 
-  momentArm.update();
+  positionVector.update();
   force1.update();
-  momentArm.display();
+  positionVector.display();
   force1.display();
 
+  //calculate angle phi between position and force vector (limited to (-90,90)degrees)
+  //so absolute value of x so that y value is used to give proper sign/sense for torque calculation
+  phi = atan((force1.target.y-force1.origin.y)/abs(force1.target.x-force1.origin.x));
+  force1mag = dist(force1.origin.x,force1.origin.y,force1.target.x,force1.target.y);
+
   //Calculate Torque
-  torque = -1*(force1.target.y-force1.origin.y).toFixed(0)*pointApplied_x.value()/10;
+  torque = -1*sin(phi)*force1mag*pointApplied_x.value()/10;
+  //when pointApplied_x.val = 100 , take to be 100mm, divide by 10 for N-cm torque calculation
+
 
   fill('black');
-  text("The blue arrow is the position vector corresponding to the moment arm",50,20);
-  text("The red arrow is the force vector",50,35);
-  text("Notice the sense change in torque vector",50,50);
-  text("Torque= "+torque.toFixed(0)+" N-cm",500,400);
-  text("Moment Arm Length= "+(pointApplied_x.value()/10).toFixed(0)+" cm",400,100);
+  text("Applied Force (F) = "+force1mag.toFixed(0)+" N-cm",100,20);
+  text("Torque = "+torque.toFixed(0)+" N-cm",100,50);
+  text("Position Vector (r) = "+(pointApplied_x.value()/10).toFixed(0)+" cm",100,100);
+  text("F",force1.target.x+15,force1.target.y+15);
+  text("r",positionVector.target.x-20,positionVector.target.y+15);
 
-
-
+//draw torque vector into or out of page at center of nut/bolt
   if (torque < 0){
-    //text("The bolt will tighten (Negative Toqrue, going into screen along -z axis)",50,400);
-    //ellipse(center.x,center.y,30,30);
     strokeWeight(3);
     line(center.x-10,center.y-10,center.x+10,center.y+10);
     line(center.x-10,center.y+10,center.x+10,center.y-10);
@@ -84,13 +91,10 @@ function draw(){
     arc(center.x, center.y, 30, 30, 0, TWO_PI);
 
   } else if (torque > 0){
-  //  text("The bolt will loosen(Positive Torque, coming out of screen along +z axis)",50,400);
     strokeWeight(3);
     ellipse(center.x, center.y, 5, 5);
     noFill();
     arc(center.x, center.y, 30, 30, 0, TWO_PI);
-  } else{
-  // text("The bolt will not move! No moment applied!",50,400);
   }
 }
 
