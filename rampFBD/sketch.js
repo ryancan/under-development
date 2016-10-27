8f356a5ed192b722e68b5c4c830d4761d865034b
ryancan <ryanvcan@gmail.com>
2016-10-26 21:38:54 -0400

    Added FBD, mu Slider

--- rampFBD/sketch.js
+++ rampFBD/sketch.js
@@ -5,22 +5,27 @@ function setup(){
   createCanvas(700,500);
   background('blue');
 
-  m = 5;//mass of block
-  g = 0.0981;//gravity
-  mu = .3;//coefficient of static friction
-  weight = new createVector(0,m*g*250);
+  m = 50;//mass of block in kg
+  g = 1.5;//gravity
+
+  muSlider = createSlider(0.1,0.9,0.5,0.1);
+  muSlider.position(100,580);
+
+  mu_s = muSlider.value();//coefficient of static friction
+  mu_k = muSlider.value()/2;//kinetic
+  weight = new createVector(0,m*g);
+  mu = mu_s; //set as static friciton to start
+
+
 
   //slider used to add/subtract y-value to apex of triangle, thus changing the angle of ramp
-  rampSlider = createSlider(-150,150,0);
+  rampSlider = createSlider(-150,200,0);
   rampSlider.position(100,550);
-
   //calculate angle of ramp
   theta = atan((400-(250-rampSlider.value))/(500));
-
   //setup ramp triangular coordinates using vectors
   rightCorner = new createVector(600,400);
   apex = new createVector(100,250-rampSlider.value());
-
   //calculate middle of incline as vector so that those coordinates can be used to draw the box
   center = new createVector((apex.x+rightCorner.x)/2,(apex.y+rightCorner.y)/2);
 
@@ -32,19 +37,19 @@ function setup(){
   boxVel = 0;
   boxPos = 0;
 
-  gravEnd = p5.Vector.add(center,weight)
+
+  gravEnd = p5.Vector.add(center,weight);
   gravVec = new Arrow(center,gravEnd);
   gravVec.color= color('red');
   gravVec.grab = false;
   gravVec.draggable = false;
   gravVec.showComponents = false;
 
-  //trying to use science lib fbd on box
-//  box_fbd = new FBD(center,4,true);
-
   reset = createButton('reset');
   reset.position(200,200);
   reset.mousePressed(reset2);
+
+  //frameRate(15);
 }
 
 //resets the box to middle of ramp and gives it 0 velocity and acceleration
@@ -62,24 +67,25 @@ function draw(){
   fill('green');
   rect(0,400,700,100); //just some scenic grass
 
+  mu_s = muSlider.value();//coefficient of static friction
+  mu_k = muSlider.value()/2;//kinetic
 
   //coordinates again of triangle ramp and box location
   rightCorner = new createVector(600,400);
   apex = new createVector(100,250-rampSlider.value());
   center = new createVector((apex.x+rightCorner.x)/2,(apex.y+rightCorner.y)/2);
-
   //recalculate angle in case ramp geometry has changed
   theta = atan((rightCorner.y-apex.y)/(rightCorner.x-apex.x));
 
+  if (boxVel == 0){
+    mu = mu_s;
+  } else{
+    mu = mu_k;
+  };
 
-  friction = m*mu*g*cos(theta);
+  normalForce_= m*g*cos(theta);
+  friction = mu*normalForce_;
   rampGrav = m*g*sin(theta);
-
-  gravVec = new Arrow(center,gravEnd);
-  gravVec.color= color('red');
-  gravVec.grab = false;
-  gravVec.draggable = false;
-  gravVec.showComponents = false;
 
   //keeps box in equilibrium untill gravity overcomes friction
   if (friction > rampGrav){
@@ -92,59 +98,129 @@ function draw(){
       boxVel = boxVel + boxAccl.x;
   } else{
     boxVel = 0;
-  }
-  boxAccl = new createVector((rampGrav-friction)/m,0);
-  //vector was not used for velocity here because the box only moves uni-directionly along ramp
-  //this however should be changed and vectors should be used.....
+  };
 
-  boxPos = boxPos + boxVel;
+  netForce_ = rampGrav - friction;
 
-//  if (friction = rampGrav){
-  //  boxVel = 0;
-//  };
+  //set position vector to be origin of all arrow objects
+  gravCenter = new createVector(center.x+(cos(theta)*(boxPos))+(sin(theta)*40), center.y-40+(sin(theta)*(boxPos+10)));
 
-  //draw the ramp
-  fill('tan');
-  triangle(100,400,apex.x,apex.y,rightCorner.x,rightCorner.y);
+  gravEnd = p5.Vector.add(gravVec.origin,weight);
+  gravVec = new Arrow(gravCenter,gravEnd);
 
-  //angle text output
-  //how to limit sig figs ???
-  fill('white');
-  text("Angle of Ramp = "+theta.toFixed(2)+" degrees",400,450);
+  normalForce = new createVector(normalForce_*sin(theta),-normalForce_*cos(theta));
+  normEnd = p5.Vector.add(gravCenter,normalForce);
+  normVec = new Arrow(gravCenter,normEnd);
 
-  rectMode(CENTER);
-  fill('brown');
-  translate(center.x,center.y);//translates coordinate to center so box rotates correctly
-  rotate(theta);//rotate box to be flush with ramp according to angle theta
-  rect(boxPos,-40,80,80);//(0,0) is new position of center
-  stroke('black');
-  strokeWeight(15);
-  //draw a stop for the box to prevent any weird transitions of box moving from ramp to ground
-  line(250,0,250,-40);
+
+if (friction == rampGrav){
+  frictionForce = new createVector(-rampGrav*cos(theta),-rampGrav*sin(theta));
+} else if (boxPos > 200) {
+  frictionForce = new createVector(-rampGrav*cos(theta),-rampGrav*sin(theta));
+} else {
+  frictionForce = new createVector(-friction*cos(theta),-friction*sin(theta));
+};
+
+  frictionEnd = p5.Vector.add(gravCenter,frictionForce);
+  frictionVec = new Arrow(gravCenter,frictionEnd);
+
+  netForce = new createVector(cos(theta)*netForce_,sin(theta)*netForce_);
+  netForceEnd = p5.Vector.add(gravCenter,netForce);
+  netForceVec = new Arrow(gravCenter,netForceEnd);
+
+if (boxPos > 200){
+  frictionVec.color = ('purple'); //turn into normal force from stopper
+} else if(mu == mu_k){
+  frictionVec.color = color('green');
+} else {
+  frictionVec.color = color('orange');
+};
+
+  frictionVec.grab = false;
+  frictionVec.draggable = false;
+  frictionVec.showComponents = false;
+
+  gravVec.color= color('red');
+  gravVec.grab = false;
+  gravVec.draggable = false
+  gravVec.showComponents = false;
+
+  normVec.color = color('purple');
+  normVec.grab = false;
+  normVec.draggable = false;
+  normVec.showComponents = false;
+
+  netForceVec.color= color('orange');
+  netForceVec.grab = false;
+  netForceVec.draggable = false
+  netForceVec.showComponents = false;
+  netForceVec.width = 10;
 
   //stops the box from moving when it reaches the stop
   if(boxPos > 200){
     boxPos=205;
     boxVel = 0;
     boxAccl = new createVector(0,0);
+    netForce_ = 0;
+    friction = rampGrav;
   };
 
-//  box_fbd.mag = [550*g,friction,rampGrav,friction/mu];
-//  box_fbd.direction = [90,180+theta,-1*theta,theta];
-//  box_fbd.xoffsets = [0,0,0,0];
-//  box_fbd.yoffsets = [0,0,0,0];
-//  box_fbd.labels = ['force 1','force 2','force 3','force 4'];
+  boxAccl = new createVector((rampGrav-friction)/m,0);
+  //vector was not used for velocity here because the box only moves uni-directionly along ramp
+  //this however should be changed and vectors should be used.....
 
-  //fill('red');
-//  box_fbd.update();
-//  box_fbd.display();
+  boxPos = boxPos + boxVel;
 
-// gravVec.target = p5.Vector.add(center,p5.Vector.mult(weight,1));
+  //draw the ramp
+  fill('tan');
+  triangle(100,400,apex.x,apex.y,rightCorner.x,rightCorner.y);
 
- gravVec.update();
- gravVec.display();
+  //angle text output
+  fill('white');
+  text("Angle of Ramp = "+theta.toFixed(2)+" degrees",300,450);
+  text("Net Force = "+netForce_.toFixed(2)+" Newtons",500,475);
+if (mu == mu_s){
+  text("Mu = "+mu+" Static",300,475);
+} else{
+  text("Mu = "+mu+" Kinetic",300,475);
+};
 
- console.log(boxVel)
- //console.log(gravVec.origin)
- //console.log(gravVec.target)
+
+push();
+  rectMode(CENTER);
+  fill('brown');
+
+  translate(center.x,center.y);//translates coordinate to center so box rotates correctly
+
+  rotate(theta);//rotate box to be flush with ramp according to angle theta
+
+  rect(boxPos,-40,80,80);//(0,0) is new position of center
+
+  stroke('black');
+  strokeWeight(15);
+  //draw a stop for the box to prevent any weird transitions of box moving from ramp to ground
+  line(250,0,250,-40);
+pop();
+
+push();
+gravVec.update();
+gravVec.display();
+normVec.update();
+normVec.display();
+frictionVec.update();
+frictionVec.display();
+netForceVec.update();
+if (netForce_ > 0.001){
+  netForceVec.display();
+};
+pop();
+
+
+//  console.log(friction)
+//  console.log(rampGrav)
+//    console.log(mu)
+//    console.log(netForce_)
+//    console.log(boxVel)
+//    console.log(boxVel)
+
 }
